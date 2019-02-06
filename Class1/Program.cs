259630876acb421e896f99e5222e3ad9d68d7de0
Anis Beyzaee <anisbeyzaee@gmail.com>
2019-02-06 11:11:20 -0800

    moving into Fol

---
+++ Class1/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace Class1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            connect("SALAM HAJI");
+
+
+
+            void connect(String path)
+            {
+                ProcessStartInfo s = new ProcessStartInfo();
+                //s.FileName = @"C:\Program Files\Java\jre1.8.0_191\bin\java.exe";
+                s.FileName = @"JoeClassifier.java";
+                s.WorkingDirectory = @"C:\Temp";
+
+
+                s.Arguments = @"JoeClassifier " + path; // C:\Temp\ForestImage.png";
+
+                s.UseShellExecute = false;
+
+                s.RedirectStandardOutput = true;
+
+                s.RedirectStandardError = true;
+
+                s.CreateNoWindow = true;
+
+
+
+                Process process = new Process();
+
+                process.StartInfo = s;
+
+                process.Start();
+
+
+
+
+
+                while (!process.StandardOutput.EndOfStream)
+
+                {
+
+                    string isFire = process.StandardOutput.ReadLine();
+
+
+
+                    System.Windows.Forms.MessageBox.Show("isFire: " + isFire);
+
+                    break;
+
+                }
+            }
+
+        }
+    }
+}
